1688db8b659a237ee3c9e574aa360edd73729806
LukasKunkel <165786238+LukasKunkel@users.noreply.github.com>
2024-04-29 14:35:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 import math
 import numpy as np
 import csv
+from math import log
 from PyQt5 import QtWidgets
 from PyQt5.uic import loadUi
 from PyQt5.QtWidgets import QMainWindow, QApplication, QTableWidget  # , QMessageBox
@@ -9,20 +10,22 @@ from astroquery.gaia import Gaia
 from astroquery.vizier import Vizier
 from astropy.coordinates import Angle
 from matplotlib import pyplot
+from scipy.stats import linregress as linreg
 
 from LC import LightCurve  # from lightcurves.LC import LightCurve
 
 # Factors:
 Ra = -0.02275; Rb = 0.3961; Rc = -0.1243; Rd = -0.01396; Re = 0.003775
 Va = -0.02704; Vb = 0.01424; Vc = -0.2156; Vd = 0.01426
-Ba = -0.04749; Bb = -0.0124; Bc = -0.2901; Bd = 0.02008; W = ((-3) ** (1 / 2)) / 2
-Q = 3 * Bd; R = Bc ** 2 - Q * Bb; S = - Bc / Q
+Ba = 0.1231; Bb = -2.6513; Bc = 0.0248; Bd = 0.0262
 
 date = []
 mag = []
 mag_error = []
+mag2 = []
+mag_error2 = []
 
-with open('PSD data/BL Lac 1+5 R.csv', 'r') as file:
+with open('PSD data/BL Lac R.CSV', 'r') as file:
     csv_reader = csv.reader(file, delimiter=';')
     next(csv_reader)
     for row in csv_reader:
@@ -31,22 +34,38 @@ with open('PSD data/BL Lac 1+5 R.csv', 'r') as file:
         mag_error.append(float(row[2]))
 
 time = np.array(date)
-flux = np.array(mag)
-flux_error = np.array(mag_error)
+flux = 10 ** (-np.array(mag)/2.5)
+flux_error = log(10) * flux * np.array(mag_error) / 2.5
 
 lc = LightCurve(time, flux, flux_error)
 
-np, psd = lc.get_lsp()
-lc.plot_nice()
+psd = lc.get_lsp()
+slope, inter, rval, pval, stder = linreg(np.log10(psd[0][:int(len(psd[0]/2))]), np.log10(psd[1][:int(len(psd[1]/2))]))
+
+ax = pyplot.axes()
+ax.tick_params(axis="both", which="both", direction='in')
+x = np.linspace(np.log10(min(psd[0][:int(len(psd[0]/2))])), np.log10(max(psd[0][:int(len(psd[0]/2))])), 10**4)
+pyplot.plot(psd[0][:int(len(psd[0]/2))], psd[1][:int(len(psd[1]/2))], color='black', label='BL Lac R', linewidth=1)
+pyplot.plot(10 ** x, 10 ** (x * slope + inter), color='crimson', label=f'linear fit, slope {slope:.6f}', linewidth=2.5)
+
+pyplot.grid(color='grey', linestyle='solid')
+pyplot.title('Lomb-Scargle periodogram')
+pyplot.xlabel('frequency [1/d]')
+pyplot.ylabel('power')
+pyplot.xscale('log')
+pyplot.yscale('log')
+pyplot.legend()
+print(slope, inter)
+#lc.plot_nice()
 pyplot.show()
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
-        loadUi("UI.ui", self)
+        loadUi("TestUI.ui", self)
 
-        self.lineEdit.setText('3C 371')
+        self.lineEdit.setText('BL Lac')
 
         for i in range(9):
             self.t1.hideColumn(i)
@@ -63,7 +82,7 @@ class MainWindow(QMainWindow):
         self.Nachkomma.setMaximum(10)
         self.Nachkomma.setValue(6)
 
-        self.Ergebnisse.setValue(99)
+        self.Ergebnisse.setValue(20)
 
         self.b1.clicked.connect(self.search)
         self.b1.setShortcut("Shift+Return")
@@ -85,7 +104,7 @@ class MainWindow(QMainWindow):
             self.Object.setText(f'{star_id}')
         radius1: int = self.doubleSpinBoxRadius.value()  # 5 min ~ 0.0833 deg ; whole frame ~ 0.6 deg
         radius = 0
-        bereich: int = self.MagBereich.value()
+        region: int = self.MagBereich.value()
         digits: int = self.Nachkomma.value()
         ygaia: bool = self.Gaia.isChecked()
         yadistance: bool = self.aDistance.isChecked()
@@ -113,9 +132,9 @@ class MainWindow(QMainWindow):
         parallax_obj = results1['parallax'][0]
         pmra_obj = results1['pmra'][0]
         pmdec_obj = results1['pmdec'][0]
-        R_obj = G_obj - Ra - (BP_obj - RP_obj) * Rb - (BP_obj - RP_obj) ** 2 * Rc - (BP_obj - RP_obj) ** 3 * Rd
+        R_obj = G_obj - Ra - (BP_obj - RP_obj) * Rb - (BP_obj - RP_obj) ** 2 * Rc - (BP_obj - RP_obj) ** 3 * Rd - (BP_obj - RP_obj) ** 4 * Re
         V_obj = G_obj - Va - (BP_obj - RP_obj) * Vb - (BP_obj - RP_obj) ** 2 * Vc - (BP_obj - RP_obj) ** 3 * Vd
-        B_obj = G_obj - Ba - (BP_obj - RP_obj) * Bb - (BP_obj - RP_obj) ** 2 * Bc - (BP_obj - RP_obj) ** 3 * Bd
+        B_obj = G_obj - Ba - (V_obj - R_obj) * Bb - (V_obj - R_obj) ** 2 * Bc - (V_obj - R_obj) ** 3 * Bd
 
         query2 = f"SELECT TOP 2000 \
                                     phot_g_mean_mag, phot_bp_mean_mag, phot_rp_mean_mag, \
@@ -134,9 +153,9 @@ class MainWindow(QMainWindow):
                                                 COORD2(EPOCH_PROP_POS({ra_obj},{dec_obj},{parallax_obj},{pmra_obj},{pmdec_obj},0,2000,2016.0)),\
                                                 {radius}) \
                                 )=1 \
-                                AND (phot_g_mean_mag >= {G_obj} - {bereich}) \
-                                AND (phot_g_mean_mag <= {G_obj} + {bereich}) \
-                                AND (phot_variable_flag != 'VARIABLE') "
+                                AND (phot_g_mean_mag >= {G_obj} - {region} ) \
+                                AND (phot_g_mean_mag <= {G_obj} + {region} ) \
+                                AND (phot_variable_flag != 'VARIABLE') "  # AND (teff_gspphot != 0)"
         job2 = Gaia.launch_job(query2)
         results2 = job2.get_results()
         ra_star = results2['ra']
@@ -144,6 +163,9 @@ class MainWindow(QMainWindow):
         G_star = results2['phot_g_mean_mag']
         BP_star = results2['phot_bp_mean_mag']
         RP_star = results2['phot_rp_mean_mag']
+        eG_star = results2['phot_g_mean_mag_error']
+        eBP_star = results2['phot_bp_mean_mag_error']
+        eRP_star = results2['phot_rp_mean_mag_error']
 
         def angular_separation(ra1, dec1, ra2, dec2):
             ra1_rad = math.radians(ra1)
@@ -157,48 +179,35 @@ class MainWindow(QMainWindow):
             separation = math.degrees(c)
             return separation
 
-        R_star = []
-        V_star = []
-        B_star = []
-        B = []; K1 = []; P = []; T = []; N1 = []; N2 = []; N3 = []
-        coeff = []
-        roots = []
+        R_star = []; V_star = []; B_star = []
+        eR_star = []; eV_star = []; eB_star = []
         angular_distance = []
         row = 0
         sterne = []
         for i in range(len(results2)):
             R_star.append(i)
             V_star.append(i)
-            K1.append(i); P.append(i); T.append(i)
-            N1.append(i); N2.append(i); N3.append(i)
-            coeff.append(i)
-            roots.append(i)
+            B_star.append(i)
+            eR_star.append(i)
+            eV_star.append(i)
+            eB_star.append(i)
             angular_distance.append(i)
             R_star[i] = G_star[i] - Ra - (BP_star[i] - RP_star[i]) * Rb - (BP_star[i] - RP_star[i]) ** 2 * Rc - (
                     BP_star[i] - RP_star[i]) ** 3 * Rd - (BP_star[i] - RP_star[i]) ** 4 * Re
             V_star[i] = G_star[i] - Va - (BP_star[i] - RP_star[i]) * Vb - (BP_star[i] - RP_star[i]) ** 2 * Vc - (
                     BP_star[i] - RP_star[i]) ** 3 * Vd
+            B_star[i] = G_star[i] - Ba - (V_star[i] - R_star[i]) * Bb - (V_star[i] - R_star[i]) ** 2 * Bc - (
+                        V_star[i] - R_star[i]) ** 3 * Bd
             angular_distance[i] = angular_separation(ra_star[i], dec_star[i], ra_obj, dec_obj)
-            K1[i] = -(G_star[i] - V_star[i]) + Ba
-            P[i] = 2 * Bc ** 3 - 9 * Bd * Bc * Bb + 27 * (Bd ** 2) * K1[i]
-            T[i] = (((P[i] ** 2 - 4 * R ** 3) ** 1 / 2 + P[i]) / 2) ** 1 / 3
-            N1[i] = S + (T[i] / Q) - (R / (Q * T[i]))
-            N2[i] = S + ((T[i] * (0.5 + W)) / Q) + (((0.5 - W) * R) / (Q * T[i]))
-            N3[i] = S + ((T[i] * (0.5 - W)) / Q) + (((0.5 + W) * R) / (Q * T[i]))
-            coeff[i] = [K1[i], Bb, Bc, Bd]
-            # roots[i] = np.roots(coeff[i]) + V_star[i]
-
-        #  for i in range(3):
-            #  print(roots[i])
-            #  print(N1[i])
-            #  print(Q, R, S)
-            #  print(T[i])
-
-        # for root in roots:
-        #  B.append(root[2])
-        for i in range(len(results2)):
-            B_star.append(i)
-            B_star[i] = 0  # B[i]
+            eR_star[i] = ((eG_star[i]) ** 2 + ((-Rb - 2 * Rc * (BP_star[i] - RP_star[i]) - 3 * Rd * (
+                        BP_star[i] - RP_star[i]) ** 2 - 4 * Re * (BP_star[i] - RP_star[i]) ** 3) ** 2) * (
+                                      (eBP_star[i]) ** 2 + (eRP_star[i]) ** 2)) ** 0.5
+            eV_star[i] = ((eG_star[i]) ** 2 + (
+                        (-Vb - 2 * Vc * (BP_star[i] - RP_star[i]) - 3 * Vd * (BP_star[i] - RP_star[i]) ** 2) ** 2) * (
+                                      (eBP_star[i]) ** 2 + (eRP_star[i]) ** 2)) ** 0.5
+            eB_star[i] = ((eG_star[i]) ** 2 + (
+                        (-Bb - 2 * Bc * (V_star[i] - R_star[i]) - 3 * Bd * (V_star[i] - R_star[i]) ** 2) ** 2) * (
+                                      (eV_star[i]) ** 2 + (eR_star[i]) ** 2)) ** 0.5
 
         if len(results2) == 0:
             self.l6.setText(f"No Non-Variable stars found around {objectname} within a radius of: {radius:.2f} deg.")
@@ -212,16 +221,16 @@ class MainWindow(QMainWindow):
                 stuff = ergebnisse
             for i in range(stuff):
                 sterne.append(i)
-                sterne[i] = {"Ra": f"{ra_star[i]:.8f}", "Dec": f"{dec_star[i]:.8f}",
+                sterne[i] = {"Ra": f"{ra_star[i]:.5f}", "Dec": f"{dec_star[i]:.5f}",
                              "G": f"{G_star[i]:.{digits}f}",
                              "BP": f"{BP_star[i]:.{digits}f}",
                              "RP": f"{RP_star[i]:.{digits}f}",
-                             "R": f"{R_star[i]:.{digits}f}",
-                             "V": f"{V_star[i]:.{digits}f}",
-                             "B": f"{B_star[i]:.{digits}f}",
+                             "R": f"{R_star[i]:.{digits}f} ({eR_star[i]:.{digits}f})",
+                             "V": f"{V_star[i]:.{digits}f} ({eV_star[i]:.{digits}f})",
+                             "B": f"{B_star[i]:.{digits}f} ({eB_star[i]:.{digits}f})",
                              "Dist": f"{angular_distance[i]:.{digits}f}"}
             self.l6.setText(f'A total of {len(results2)} stars were found. \n'
-                            f'Their magnitudes are in the range of G: {G_obj:.2f} +/- {bereich:.2f} mag. \n'
+                            f'Their magnitudes are in the range of G: {G_obj:.2f} +/- {region:.2f} mag. \n'
                             f'Non-Variable stars around {objectname} within a radius of: {radius:.2f} deg.')
             self.l6.adjustSize()
             sorted_stars = sorted(sterne, key=lambda x: x["Dist"])
@@ -254,7 +263,7 @@ class MainWindow(QMainWindow):
         self.t1.resizeRowsToContents()
         self.t1.resizeColumnsToContents()
         self.t1.setSizeAdjustPolicy(QTableWidget.AdjustToContents)
-        self.textEdit.setText(f'Ra: {ra_obj:.4f}; Dec: {dec_obj:.4f} \n'
+        self.textEdit.setText(f'ra: {ra_obj:.6f} ; dec: {dec_obj:.6f} \n'
                               f'G: {G_obj:.6f}; BP: {BP_obj:.6f}; RP: {RP_obj:.6f} \n'
                               f'R: {R_obj:.6f};  V: {V_obj:.6f};   B: {B_obj:.6f} \n'
                               f'Variable: {var_obj}')
@@ -269,10 +278,10 @@ app = QApplication(sys.argv)
 mainwindow = MainWindow()
 widget = QtWidgets.QStackedWidget()
 widget.addWidget(mainwindow)
-widget.setGeometry(100, 100, 900, 700)
-widget.setWindowTitle("Star finder")
+widget.setGeometry(100, 100, 750, 700)
+widget.setWindowTitle("NV-StarFinder")
 widget.show()
 try:
-    sys.exit(app.exec_())
+    pass #sys.exit(app.exec_())
 finally:
     pass
